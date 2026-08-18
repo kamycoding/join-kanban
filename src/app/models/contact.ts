e391df1c68059bbc90460a39d5508a8c999f5c4b
Tobias Illner <tobias.illner@hotmail.de>
2026-08-18 09:52:33 +0200

    feat: created Interface and Services for contact

---
+++ src/app/models/contact.ts
@@ -0,0 +1,15 @@
+export interface Contact {
+  id: string;
+  created_at: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  color: string;
+  updated_at: string;
+}
+
+export type NewContact = Omit<
+  Contact,
+  'id' | 'created_at' | 'updated_at'
+>;

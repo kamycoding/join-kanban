3758ad9f1dbc7fea7d8166edf7d1909aa108443e
Tobias Illner <tobias.illner@hotmail.de>
2026-08-18 10:07:22 +0200

    feat: Interface and Service finish

--- src/app/services/contact.ts
+++ src/app/services/contact.ts
@@ -1,9 +1,27 @@
 import { Service, signal } from '@angular/core';
 import { createClient, RealtimeChannel } from '@supabase/supabase-js';
+import { Contact } from '../models/contact';
 
 @Service()
 export class ContactService {
-    supabaseUrl = 'https://oslontuktgqzrsbetdur.supabase.co';
-    supabaseKey = 'sb_publishable_wneSRSKtk-Tg8TmwtheD5w_nX9Cj4ue';
-    supabase = createClient(this.supabaseUrl, this.supabaseKey);
+    private readonly supabaseUrl = 'https://oslontuktgqzrsbetdur.supabase.co';
+    private readonly supabaseKey = 'sb_publishable_wneSRSKtk-Tg8TmwtheD5w_nX9Cj4ue';
+    private readonly supabase = createClient(this.supabaseUrl, this.supabaseKey);
+
+    readonly contacts = signal<Contact[]>([]);
+
+    async getContacts(){
+        const {data, error} = await this.supabase
+            .from('contacts')
+            .select('*')
+            .order('first_name', { ascending: true})
+            .order('last_name', { ascending: true });
+        
+        if(error) {
+            console.error('Kontakte konnten nicht geladen werden:', error);
+            return;
+        }
+
+        this.contacts.set(data as Contact[]);
+    }
 }

05fd781239bde65b995d98ac3dba642629cd2300
Maik Radke <maikmuc80@gmail.com>
2026-08-19 11:02:29 +0200

    feat: routes and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Summary } from './pages/summary/summary';
+import { AddTask } from './pages/add-task/add-task';
+import { Board } from './pages/board/board';
+import { Contacts } from './pages/contacts/contacts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',         redirectTo: 'summary', pathMatch: 'full' },
+    { path: 'summary',  component: Summary },
+    { path: 'add-task', component: AddTask },
+    { path: 'board',    component: Board },
+    { path: 'contacts', component: Contacts }
+];
